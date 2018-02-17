82b056768ee31ba76c118a47cebea16c6309af29
Christopher Xu <christopher.xu@live.com>
2018-02-17 18:34:31 -0500

    added install package feature using scrips

--- install_packages.m
+++ install_packages.m
@@ -19,21 +19,25 @@
 :- import_module
     manifest,
     util.
-
+/*
 install_packages(!IO) :-
     manifest_from_file(MaybeManifest, !IO),
     (
       MaybeManifest = ok(Manifest),
-      map.foldl(install_package, Manifest ^ dependencies, !IO)
+      map.foldl(clone_package, Manifest ^ dependencies, !IO)
     ;
       MaybeManifest = error(ErrorMsg),
       io_write_error(ErrorMsg, !IO),
       io.set_exit_status(1, !IO)
     ).
 
-:- pred install_package(string::in, string::in, io::di, io::uo) is det.
-install_package(Key, Value, !IO) :-
+:- pred clone_package(string::in, string::in, io::di, io::uo) is det.
+clone_package(Key, Value, !IO) :-
     system("git clone " ++ Value, !IO).
+*/
+install_packages(!IO) :-
+    Key = "json",
+    system("./installing_files.sh " ++ Key, !IO).
 
 :- pred system(string::in, io::di, io::uo) is det.
 :- pragma foreign_proc("C", system(String::in, IO0::di, IO::uo), [will_not_call_mercury, promise_pure], 
